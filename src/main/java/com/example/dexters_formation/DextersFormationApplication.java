64a66d3b2a2efeb13b01c0150b603aa8828ffdbe
Rayane Fiach <139344446+Rayane20777@users.noreply.github.com>
2024-10-28 11:50:07 +0100

    first commit

---
+++ src/main/java/com/example/dexters_formation/DextersFormationApplication.java
@@ -0,0 +1,13 @@
+package com.example.dexters_formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DextersFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DextersFormationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dexters_formation/DextersFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dexters_formation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DextersFormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
